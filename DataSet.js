8aab6666d392882e245b307b2075cf4ffb3c12c6
humbold2020 <humbold4u@gmail.com>
2020-09-09 00:00:12 -0400

    adding sorting functionality via merge sort

--- DataSet.js
+++ DataSet.js
@@ -6,14 +6,10 @@ class DataSet {
     }
     sort() {
         console.log(mergeSort(this.dataset));
+        return mergeSort(this.dataset);
     }
 }
-const data = [];
-const randomize = () => Math.floor(Math.random() * 40);
-for (let i = 0; i < 15; i++) {
-    data.push(randomize());
-}
-const dataSet = new DataSet(data);
-dataSet.sort();
+
+
 
 
