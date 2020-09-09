822f205365a2ee64df1d6cfc5c0ac68661703d10
humbold2020 <humbold4u@gmail.com>
2020-09-09 01:11:02 -0400

    dataset

--- DataSet.js
+++ DataSet.js
@@ -10,16 +10,45 @@ class DataSet {
     }
 
     getMean() {
-        const add = population => {
-            return population.reduce((acc, cur) => {
-                return acc + cur;
-            });
+        const added = add(this.dataset);
+        console.log('Mean:', (added / this.dataset.length));
+        return added / this.dataset.length;
+    }
+
+    getMedian() {
+        if (this.dataset.length % 2 === 0) {
+            const sortedData = mergeSort(this.dataset);
+            const medMean = [];
+            const data1 = Math.floor((sortedData.length - 1) / 2);
+            const data2 = Math.floor(sortedData.length / 2);
+            medMean.push(sortedData[data1], sortedData[data2]);
+            const added = add(medMean);
+            const median = added / medMean.length;
+            console.log('median:',median)
+        } else {
+            const sortedData = mergeSort(this.dataset);
+            const median = (sortedData.length - 1) / 2;
+            console.log('Median:', sortedData[median]);
+            return sortedData[median];
         }
-        const added = add(population);
-        console.log('Mean:', (added / population.length));
-        return added / population.length;
     }
 }
+
+const add = population => {
+    return population.reduce((acc, cur) => {
+        return acc + cur;
+    });
+}
+const data = [];
+const randomize = () => Math.floor(Math.random() * 1000);
+for (let i=0; i<14; i++) {
+    data.push(randomize());
+}
+const dataSet = new DataSet(data);
+dataSet.sort();
+dataSet.getMean();
+dataSet.getMedian();
+
 
 
 
