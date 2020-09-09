859049d1dac4fdac8b10e30b8912c5f23a984296
humbold2020 <humbold4u@gmail.com>
2020-09-09 00:04:29 -0400

    updated console log

--- DataSet.js
+++ DataSet.js
@@ -5,8 +5,19 @@ class DataSet {
         this.size = 0;
     }
     sort() {
-        console.log(mergeSort(this.dataset));
+        console.log('Sorted Data Set:',mergeSort(this.dataset));
         return mergeSort(this.dataset);
+    }
+
+    getMean() {
+        const add = population => {
+            return population.reduce((acc, cur) => {
+                return acc + cur;
+            });
+        }
+        const added = add(population);
+        console.log('Mean:', (added / population.length));
+        return added / population.length;
     }
 }
 
