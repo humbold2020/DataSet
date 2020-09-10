5f1bee7e2ca7883aeb00984a594b0107adfff812
humbold2020 <humbold4u@gmail.com>
2020-09-10 00:19:09 -0400

    Fixed function so that can either return diagram, or a list of the manipulated quantitative data

--- diagrams.js
+++ diagrams.js
@@ -1,17 +1,17 @@
-const smplBellCurve = (x, s) => {
-    firstDevMinus = x - s;
-    firstDevPlus = x + s;
-    secondDevMinus = x - (2 * s);
-    secondDevPlus = (2 * s) + x;
-    thirdDevMinus =x - (3 * s);
-    thirdDevPlus = (3 * s) + x; 
+const smplBellCurve = (x, s, printDiagram = false) => {
+    let firstDevMinus = x - s;
+    let firstDevPlus = x + s;
+    let secondDevMinus = x - (2 * s);
+    let secondDevPlus = (2 * s) + x;
+    let thirdDevMinus =x - (3 * s);
+    let thirdDevPlus = (3 * s) + x; 
     firstDevMinus = parseFloat(firstDevMinus.toFixed(2));
     firstDevPlus = parseFloat(firstDevPlus.toFixed(2));
     secondDevMinus = parseFloat(secondDevMinus.toFixed(2));
     secondDevPlus = parseFloat(secondDevPlus.toFixed(2));
     thirdDevMinus = parseFloat(thirdDevMinus.toFixed(2));
     thirdDevPlus = parseFloat(thirdDevPlus.toFixed(2));
-    
+    if (printDiagram === true) {
     console.log(['\n',
     ['                                                       .                                   '], '\n',
     ['                                                   .   |   .                               '], '\n',
@@ -26,8 +26,10 @@ const smplBellCurve = (x, s) => {
     ['                                       .               |                .                  '], '\n',
     ['                                   .                   |                    .              '], '\n',
     [`            x - 3s : ${thirdDevMinus}    .                         |                           . x + 3s : ${thirdDevPlus}`], '\n'])
+    } else {
     variables = [firstDevMinus, firstDevPlus , secondDevMinus, secondDevPlus, thirdDevMinus, thirdDevPlus];
     return variables;
+    }
 }
 
     module.exports = smplBellCurve;
